d419de3c5c1a71b4ebb1ca9b736504a6ffcf8941
micha <micha@librelight.de>
2021-12-13 10:10:15 +0100

    add: univers inmap like input univers is 7 to 0

---
+++ map14_in_to0.sh
@@ -0,0 +1,1 @@
+python3 ArtNetProcessor.py -s 10.10.10.255 -r 2.0.0.14 --inmap 0
---
+++ start_ASP.sh
@@ -0,0 +1,3 @@
+echo "sleep 5"
+#sleep 5
+python3 /home/pi/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255
