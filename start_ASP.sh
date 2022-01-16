b47542ad4624df83bec5c1ce689eccd8805f9fab
micha <micha@librelight.de>
2022-01-16 18:32:09 +0100

    fix: gid 20, add: wlan-AP

--- start_ASP.sh
+++ start_ASP.sh
@@ -5,7 +5,7 @@ netns=$(ip netns identify $$)
 
 python3 /home/pi/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255 netns=$netns
 if [ $? != 0 ]; then
-     #python3 /home/user/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255 netns=$netns
-     python3 /home/micha/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255 netns=$netns
+     python3 /home/user/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255 netns=$netns
+     #python3 /home/micha/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255 netns=$netns
   
 fi
