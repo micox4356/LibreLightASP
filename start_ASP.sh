b789d7fd90880799a3667689f32bd403d85a383b
user <user@debian-BULLSEYE-live-builder-AMD64>
2022-01-24 19:22:37 +0100

    cleanup

--- start_ASP.sh
+++ start_ASP.sh
@@ -1,11 +1,6 @@
 echo "sleep 5"
 #sleep 5
+path="/opt/LibreLight"
 netns=$(ip netns identify $$)
 
-
-python3 /home/pi/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255 netns=$netns
-if [ $? != 0 ]; then
-     python3 /home/user/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255 netns=$netns
-     #python3 /home/micha/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255 netns=$netns
-  
-fi
+python3 $path/ASP/ArtNetProcessor.py --recive 10. --sendto 2.255.255.255 netns=$netns
