b4754d64b941ad5a48af8fac550d31b24fb06728
micha <micha@uxsrv.de>
2022-01-30 22:04:32 +0100

    fix: screen quit

--- setup_dmx.sh
+++ setup_dmx.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
+# to execute as cron job @reboot with an admin user ! not secure !
 
+sudo su user -c 'screen -ls'
+sudo su user -c 'screen -XS DMX quit | echo ""'
+sleep 5
 sudo sh /opt/LibreLight/ASP/netns.sh 25
 sudo ip netns exec blue225 su user -c 'screen -d -m -S DMX sh /opt/LibreLight/ASP/start.sh'
 #screen -r DMX
