6f0306b9154b47cd31c46708e9078b82aeb9e52a
micha <micha@librelight.de>
2022-02-10 23:32:24 +0100

    fix:

--- monitor/setup_dmx.sh
+++ monitor/setup_dmx.sh
@@ -5,4 +5,5 @@ sudo su user -c 'screen -ls'
 sudo su user -c 'screen -XS DMX quit | echo ""'
 sleep 5
 sudo sh /opt/LibreLight/ASP/netns.sh 25
-sudo ip netns exec blue225 su user -c 'screen -d -m -S DMX sh loop.sh'
+sudo ip netns exec blue225 su user -c 'screen -d -m -S DMX sh /opt/LibreLight/ASP/monitor/loop.sh'
+#sudo ip netns exec blue225 su user -c 'screen -d -m -S DMX sh loop.sh'
