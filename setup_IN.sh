7038e0b74d628e33f03eea2f7f69a5378de08596
micha <micha@librelight.de>
2022-09-17 09:58:57 +0200

    fix netns exec with sudo

--- setup_IN.sh
+++ setup_IN.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 
-sudo sh /opt/LibreLight/ASP/netns.sh 24 
-sudo ip netns exec blue224 su user -c 'screen -d -m -S ASP_IN sh /opt/LibreLight/ASP/map14_in_to0.sh'
+#sudo sh /opt/LibreLight/ASP/netns.sh 24 
+#sudo ip netns exec blue224 su user -c 'screen -d -m -S ASP_IN sh /opt/LibreLight/ASP/map14_in_to0.sh'
+
+sudo /opt/netns/create inmap14
+/opt/netns/exec --id=inmap14 --cmd="screen -d -m -S ASP_IN sh /opt/LibreLight/ASP/map14_in_to0.sh"
+
