52107da5016c187e6c2dda34582453ecb8da9c4f
micha <micha@librelight.de>
2022-11-14 21:06:10 +0100

    cleanup files oszi and grid

--- monitor/setup_dmx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# to execute as cron job @reboot with an admin user ! not secure !
-
-sudo su user -c 'screen -ls'
-sudo su user -c 'screen -XS DMX quit | echo ""'
-sleep 5
-sudo sh /opt/LibreLight/ASP/netns.sh 25
-sudo ip netns exec blue225 su user -c 'screen -d -m -S DMX sh /opt/LibreLight/ASP/monitor/loop.sh'
-#sudo ip netns exec blue225 su user -c 'screen -d -m -S DMX sh loop.sh'
--- monitor/start_oszi.sh
+++ monitor/start_oszi.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash 
 #sleep 3;
 sudo /opt/netns/create 18
-/opt/netns/exec --id=18 --cmd="xterm -e screen python3 /opt/LibreLight/ASP/monitor/oszi_grid.py" & 
+CMD="python3 /opt/LibreLight/ASP/monitor/oszi_grid.py"  
+/opt/netns/exec --id=18 --cmd="xterm -e screen $CMD" &
 
 
 
