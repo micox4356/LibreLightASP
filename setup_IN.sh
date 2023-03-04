55598e5d6d3d71874a2555500a2102d0f28cc332
micha <micha@librelight.de>
2023-03-04 21:32:43 +0100

    fix: ArtNet input merge (ip 114)

--- setup_IN.sh
+++ setup_IN.sh
@@ -3,6 +3,6 @@
 #sudo sh /opt/LibreLight/ASP/netns.sh 24 
 #sudo ip netns exec blue224 su user -c 'screen -d -m -S ASP_IN sh /opt/LibreLight/ASP/map14_in_to0.sh'
 
-sudo /opt/netns/create inmap14
-/opt/netns/exec --id=inmap14 --cmd="screen -d -m -S ASP_IN sh /opt/LibreLight/ASP/map14_in_to0.sh"
+sudo /opt/netns/create 14
+/opt/netns/exec --id=14 --cmd="screen -d -m -S ASP_IN14 sh /opt/LibreLight/ASP/map14_in_to0.sh"
 
